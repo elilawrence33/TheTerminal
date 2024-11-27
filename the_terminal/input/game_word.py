99ac971d440588b4093603a659b9c01e99862de8
elilawrence33 <elilawrence36@gmail.com>
2024-11-26 21:03:25 -0700

    More stuff n things.

--- the_terminal/input/game_word.py
+++ the_terminal/input/game_word.py
@@ -1,46 +1,41 @@
 from textblob import Word
+import nltk
 
 class GameWord: 
+    WORDNET = "wordnet"
 
-    def __init__(self, word): 
-        __word_string = word
+    def __init__(self, word, synonyms=None):
+        nltk.download(self.WORDNET)
         __word = Word(word)
         self.__create_synonyms(__word)
-
-    @property
-    def __word_string(self): 
-        return self.__word_string
     
     @property
-    def __word(self): 
+    def word(self): 
         return self.__word
     
+    @word.setter
+    def word(self, value):
+        if not value:
+            raise ValueError("Word cannot be blank")
+        self.__word = value
+
     @property
-    def __synonyms(self): 
+    def synonyms(self): 
         return self.__synonyms
     
-    @__word_string.setter
-    def __word_string(self, value): 
-        if not value:
-            raise ValueError("Word string cannot be blank")
-        self.__word_string = value
-
-    @__synonyms.setter
-    def __synonyms(self, value): 
+    @synonyms.setter
+    def synonyms(self, value): 
         if not value: 
             raise ValueError("Synonyms cannot be blank")
         self.__synonyms = value
 
-    
     # Class Methods
 
     def __create_synonyms(self, word):
         synonym_list = word.synsets
         lemmatized_syn_set = set()  # Use a set to store unique synonyms
-        for synonym in synonym_list: 
-            lemmatized_syn_set.update(synonym.lemma_names())  # Add all lemma names to the set
+        print(synonym_list)
+        # for synonym in synonym_list: 
+        #     lemmatized_syn_set.update(synonym.lemma_names())  # Add all lemma names to the set
 
-        self.__synonyms = list(lemmatized_syn_set) 
-    
-
-
+        # self.__synonyms = list(lemmatized_syn_set) 
--- the_terminal/main.py
+++ the_terminal/main.py
@@ -1,6 +1,5 @@
 from input.input_handler import InputHandler
 from input.game_word import GameWord
-from textblob import TextBlob
 
 def start(): 
     game_word = GameWord("hello")
